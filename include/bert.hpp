605bd81cd524ff4fa5a36f8de3aba78a2cd1ccc7
Rüdiger Sonderfeld <ruediger@c-plusplus.de>
2009-11-02 00:29:43 +0100

    started implementing complex bert types API

--- include/bert.hpp
+++ include/bert.hpp
@@ -364,6 +364,41 @@ namespace bert {
     i = detail::set_size(len, ++i);
     return std::copy(data.begin(), data.end(), i);
   }
+
+  // BERT Specific Stuff (non erlang term formats)
+  namespace detail {
+    template<typename Range>
+    bool is_bert_atom(Range &r) {
+      return get_type(r) == ATOM_EXT && get_atom(r) == "bert";
+    }
+  }
+
+  /**
+   * Checks if the next data is a BERT Tuple.
+   * BERT Tuple are Erlang tuple beginning with the atom "bert".
+   * They are used to extend the default erlang binary format to support
+   * more or different data strucutes.
+   */
+  template<typename Range>
+  bool is_bert_tuple(Range r) {
+    if(get_type(r) == SMALL_TUPLE_EXT) {
+      byte_t const size = get_small_tuple_size(r);
+      if(size > 0) {
+        return detail::is_bert_atom(r);
+      }
+    }
+    else if(get_type(r) == LARGE_TUPLE_EXT) {
+      boost::uint32_t const size = get_large_tuple_size(r);
+      if(size > 0) {
+        return detail::is_bert_atom(r);
+      }
+    }
+    return false;
+  }
+
+  enum complex_type_t {
+    Nil, Boolean, Dictionary, Time, Regex
+  };
 }
 
 #endif
