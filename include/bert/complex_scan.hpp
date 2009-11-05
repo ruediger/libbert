998c1e42b4fb1e93d7ae0fe3d2e7836485501947
Rüdiger Sonderfeld <ruediger@c-plusplus.de>
2009-11-05 02:00:41 +0100

    worked on the complex (BERT) types

--- include/bert/complex_scan.hpp
+++ include/bert/complex_scan.hpp
@@ -2,15 +2,30 @@
 #define LIBBERT_COMPLEX_SCAN_HPP
 
 #include "scan.hpp"
+#include <ctime>
 
 namespace bert {
-    // BERT Specific Stuff (non erlang term formats)
+  // BERT Specific Stuff (non erlang term formats)
   namespace detail {
     template<typename Range>
     bool is_bert_atom(Range &r) {
       return get_type(r) == ATOM_EXT && get_atom(r) == "bert";
     }
   }
+
+  /***
+   * checks if the next element is the atom "bert" if so
+   * it returns a the Range pointing to the element after the atom
+   * else it returns boost::none
+   */
+  template<class Range>
+  boost::optional<Range> is_next_bert_atom(Range r) {
+    if(detail::is_bert_atom(r))
+      return r;
+    else
+      return boost::none;
+  }
+
 
   /**
    * Checks if the next data is a BERT Tuple.
@@ -35,9 +50,95 @@ namespace bert {
     return false;
   }
 
+  /* i know thos BooleanTrue/BooleanFalse looks stupid (especially since both
+     are true if interpreted as bool in C++). But it is used as a type not value
+     expression in this case! Using it as a boolean value is an error and it is
+     up to the user to convert it into a proper C++ bool. Or just use parse for
+     that matter.
+  */
   enum complex_type_t {
-    Nil, Boolean, Dictionary, Time, Regex
+    Nil, BooleanTrue, BooleanFalse, Dictionary, Time, Regex
   };
+
+  /**
+   * Checks the type of a bert tuple. The Range has to point to the type atom!
+   * (the atom after the "bert" atom. So after calling is_next_bert_atom)
+   */
+  template<typename Range>
+  complex_type_t get_bert_type(Range &r) {
+    if(get_type(r) != ATOM_EXT) {
+      throw bert_exception("ill formated bert tuple");
+    }
+    atom_t type = get_atom(r);
+    if(type == "nil") {
+      return Nil;
+    }
+    else if(type == "true") {
+      return BooleanTrue;
+    }
+    else if(type == "false") {
+      return BooleanFalse;
+    }
+    else if(type == "dict") {
+      return Dictionary;
+    }
+    else if(type == "time") {
+      return Time;
+    }
+    else if(type == "regex") {
+      return Regex;
+    }
+    else {
+      throw bert_exception("unsupported bert type");
+    }
+  }
+
+  enum { Megaseconds = 0, Seconds = 1, Microseconds = 2 };
+  typedef boost::tuple<boost::int32_t, boost::int32_t, boost::int32_t> bert_time;
+
+  /// returns the time as the tuple that was received (0 -> Ms, 1 -> s, 2 -> µs)
+  bert_time get_bert_time(Range &r) {
+    boost::int32_t Mseconds;
+    if(get_type(r) == SMALL_INTEGER_EXT) {
+      Mseconds = get_small_integer(r);
+    }
+    else if(get_type(r) == INTEGER_EXT) {
+      Mseconds = get_integer(r);
+    }
+    else {
+      throw bert_exception("time type malformed (megaseconds no integer)");
+    }
+
+    boost::int32_t seconds;
+    if(get_type(r) == SMALL_INTEGER_EXT) {
+      seconds = get_small_integer(r);
+    }
+    else if(get_type(r) == INTEGER_EXT) {
+      seconds = get_integer(r);
+    }
+    else {
+      throw bert_exception("time type malformed (seconds no integer)");
+    }
+    boost::int32_t useconds;
+    if(get_type(r) == SMALL_INTEGER_EXT) {
+      useconds = get_small_integer(r);
+    }
+    else if(get_type(r) == INTEGER_EXT) {
+      useconds = get_integer(r);
+    }
+    else {
+      throw bert_exception("time type malformed (microseconds no integer)");
+    }
+    return boost::make_tuple(Mseconds, seconds, useconds);
+  }
+  /// converts the time into an std::time_t
+  std::time_t get_bert_time_as_time_t(Range &r) {
+    bert_time const time_ = get_bert_time(r);
+    return 
+      std::time_t(boost::get<Megaseconds>(time_)) * 1000000 + //10**6
+      std::time_t(boost::get<Seconds>(time_));
+    // time_t has a resolution of seconds so µs' are ignored
+  }
 }
 
 #endif
