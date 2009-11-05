4d6b2835ed13ffb2f8542533f7d2d83c316b3ee8
Rüdiger Sonderfeld <ruediger@c-plusplus.de>
2009-11-05 02:58:06 +0100

    added extended_type header

--- include/bert/extended_scan.hpp
+++ include/bert/extended_scan.hpp
@@ -2,7 +2,7 @@
 #define LIBBERT_BERT_EXTENDED_SCAN_HPP
 
 #include "scan.hpp"
-#include <boost/tuple/tuple.hpp>
+#include "extended_type.hpp"
 #include <boost/optional.hpp>
 #include <ctime>
 
@@ -52,16 +52,6 @@ namespace bert {
     return false;
   }
 
-  /* i know thos BooleanTrue/BooleanFalse looks stupid (especially since both
-     are true if interpreted as bool in C++). But it is used as a type not value
-     expression in this case! Using it as a boolean value is an error and it is
-     up to the user to convert it into a proper C++ bool. Or just use parse for
-     that matter.
-  */
-  enum extended_type_t {
-    Nil, BooleanTrue, BooleanFalse, Dictionary, Time, Regex
-  };
-
   /**
    * Checks the type of a bert tuple. The Range has to point to the type atom!
    * (the atom after the "bert" atom. So after calling is_next_bert_atom)
@@ -94,9 +84,6 @@ namespace bert {
       throw bert_exception("unsupported bert type");
     }
   }
-
-  enum { Megaseconds = 0, Seconds = 1, Microseconds = 2 };
-  typedef boost::tuple<boost::int32_t, boost::int32_t, boost::int32_t> bert_time;
 
   /// returns the time as the tuple that was received (0 -> Ms, 1 -> s, 2 -> µs)
   template<typename Range>
