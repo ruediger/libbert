e1ce51b8fdc7af579f308c2f786dbc2722b338ca
Rüdiger Sonderfeld <ruediger@c-plusplus.de>
2009-11-05 02:34:47 +0100

    renamed complex to extended (^2)

--- include/bert.hpp
+++ include/bert.hpp
@@ -1,11 +1,12 @@
 #ifndef LIBBERT_BERT_HPP
 #define LIBBERT_BERT_HPP
 
-//#include "bert/complex_format.hpp"
-#include "bert/complex_scan.hpp"
+//#include "bert/extended_format.hpp"
+#include "bert/extended_scan.hpp"
 #include "bert/format.hpp"
 #include "bert/scan.hpp"
 
+//#include "bert/extended_parse.hpp"
 #include "bert/parse.hpp"
 
 #endif
