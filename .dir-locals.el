6ac7975a5d40e3428e91e6dd575f076f5ef2f593
Rüdiger Sonderfeld <ruediger@c-plusplus.de>
2013-10-17 16:12:15 +0200

    Updated .dir-locals.el.  (M)
    `coding' can't be specified and `compile-command' is always unsafe.
    Better use something else.
    
    Signed-off-by: Rüdiger Sonderfeld <ruediger@c-plusplus.de>

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,2 @@
-((nil . ((indent-tabs-mode . nil)
-         (coding . utf-8)))
- (c++-mode . ((c-basic-offset . 2)
-              (compile-command . (concat "cd " default-directory " && make")))))
+((nil . ((indent-tabs-mode . nil)))
+ (c++-mode . ((c-basic-offset . 2))))
